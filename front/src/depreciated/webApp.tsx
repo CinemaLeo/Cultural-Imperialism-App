53b38831b208c8cf6503faa88aac0b7ee1aa690f
CinemaLeo <lbgb@hotmail.co.uk>
2025-04-19 16:13:49 +0800

    Build settings applied

--- front/src/depreciated/webApp.tsx
+++ front/src/depreciated/webApp.tsx
@@ -2,10 +2,10 @@ import "../App.css";
 import "../index.css";
 
 import { TranslationProvider } from "../depreciated/translationContext.tsx";
-import Grid from "./grid.tsx";
-import TranslationComponent from "./translation.tsx";
-import MainDisplay from "./main_display.tsx";
-import SideBySideDisplay from "./side_by_side_display.tsx";
+import Grid from "../components/grid.tsx";
+import TranslationComponent from "../components/translation.tsx";
+import MainDisplay from "../components/main_display.tsx";
+import SideBySideDisplay from "../components/side_by_side_display.tsx";
 
 function WebApp() {
   return (
